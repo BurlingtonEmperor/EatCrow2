43658583c9c395dec387c4de3b797e80b9afe360
Alexander Chang <alexanderchang208@gmail.com>
2026-02-24 10:30:12 -0600

    change shortcut icon

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,5 @@
 int isHeaterOn = 0;
+int read_mode = 0;
 
 void setup() {
   Serial.begin(9600); 
@@ -22,51 +23,47 @@ void loop() {
     //   Serial.println("lowering temperature"); //
     // }
 
-    switch (incomingByte) {
-      case '0':
-        Serial.println("temp_raise");
-        break;
-      case '1':
-        Serial.println("temp_decrease");
-        break;
-      case '2':
-        Serial.println("psi_raise");
-        break;
-      case '3':
-        Serial.println("psi_decrease");
-        break;
-      case '4':
-        Serial.println("heater_on");
-        // switch (isHeaterOn) {
-        //   case 0:
-        //     isHeaterOn = 1;
-        //     break;
-        //   case 1:
-        //     Serial.println("heater_already_on");
-        //     break;
-        // }
-        break;
-      case '5':
-        Serial.println("heater_off");
-        break;
-      case '6':
-        Serial.println("psi_tank_on");
-        break;
-      case '7':
-        Serial.println("psi_tank_off");
-        break;
-      case '8':
-        Serial.println("emergency_stop");
-        break;
-      case 's':
-        while(true);
-        break;
-      case 't':
-        Serial.println("TEMP CEILING");
-        break;
-      default:
-        Serial.print("Received unknown character: ");
-        Serial.println(incomingByte);
+    switch (read_mode) {
+      case 0:
+        switch (incomingByte) {
+          case '0':
+            Serial.println("temp_raise");
+            break;
+          case '1':
+            Serial.println("temp_decrease");
+            break;
+          case '2':
+            Serial.println("psi_raise");
+            break;
+          case '3':
+            Serial.println("psi_decrease");
+            break;
+          case '4':
+            Serial.println("heater_on");
+            break;
+          case '5':
+            Serial.println("heater_off");
+            break;
+          case '6':
+            Serial.println("psi_tank_on");
+            break;
+          case '7':
+            Serial.println("psi_tank_off");
+            break;
+          case '8':
+            Serial.println("emergency_stop");
+            break;
+          case 's':
+            while(true);
+            break;
+          case 't':
+            Serial.println("TEMP CEILING");
+            break;
+          default:
+            Serial.print("Received unknown character: ");
+            Serial.println(incomingByte);
+            break;
+        }
         break;
     }
   }
