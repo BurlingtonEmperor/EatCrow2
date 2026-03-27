c569005a9243f75e27f749fa38959d27e6a7720f
Alexander Chang <alexanderchang208@gmail.com>
2026-03-27 17:03:51 -0500

    fix starforge variable error on run

--- static/hard_macros.js
+++ static/hard_macros.js
@@ -189,6 +189,7 @@ function runHardMacro (macro_name) {
         switch (parseInt(read_array_macro[2])) {
           case 0:
             console.log("STARFORGE SET");
+            starforge_global_variables = [];
             let temp_run_cycle = parse_RAIDER_RASH(read_array_macro[1]);
             console.log(temp_run_cycle);
             switch (raise_raider_rash_error) {
